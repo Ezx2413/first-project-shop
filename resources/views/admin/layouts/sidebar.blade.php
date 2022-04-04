406d1a6c60b31429c573a0285dd97badf602733b
ezx2413 <ahn2413.ahn@gmail.com>
2022-04-04 20:21:24 +0430

    dashboard market category create commit

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
     <section class="sidebar-container">
         <section class="sidebar-wrapper">
 
-            <a href="#" class="sidebar-link">
+            <a href="{{ route('admin.home') }}" class="sidebar-link">
                 <i class="fas fa-home"></i>
                 <span>خانه</span>
             </a>
