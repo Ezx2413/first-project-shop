328605c60ba96ec115dbe32dfd67892e36459438
ezx2413 <ahn2413.ahn@gmail.com>
2022-04-09 12:45:43 +0430

    dashboard commit 9 4 22

--- resources/views/admin/content/page/create.blade.php
+++ resources/views/admin/content/page/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-<title>برند</title>
+<title>پیج ساز</title>
 @endsection
 
 @section('content')
@@ -10,8 +10,8 @@
     <ol class="breadcrumb">
       <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
       <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
-      <li class="breadcrumb-item font-size-12"> <a href="#">برند</a></li>
-      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد برند</li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">پیج ساز</a></li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد پیج</li>
     </ol>
   </nav>
 
@@ -21,12 +21,12 @@
         <section class="main-body-container">
             <section class="main-body-container-header">
                 <h5>
-                  ایجاد برند
+                  ایجاد پیج
                 </h5>
             </section>
 
             <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                <a href="{{ route('admin.market.brand.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+                <a href="{{ route('admin.content.page.index') }}" class="btn btn-info btn-sm">بازگشت</a>
             </section>
 
             <section>
@@ -35,17 +35,26 @@
 
                         <section class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="">نام برند</label>
+                                <label for="">عنوان </label>
                                 <input type="text" class="form-control form-control-sm">
                             </div>
                         </section>
 
+
                         <section class="col-12 col-md-6">
                             <div class="form-group">
-                                <label for="">تصویر برند</label>
-                                <input type="file" class="form-control form-control-sm">
+                                <label for="">آدرس URL</label>
+                                <input type="text" class="form-control form-control-sm">
                             </div>
                         </section>
+
+                        <section class="col-12">
+                            <div class="form-group">
+                                <label for="">محتوی</label>
+                                <textarea name="body" id="body"  class="form-control form-control-sm" rows="6"></textarea>
+                            </div>
+                        </section>
+
                         <section class="col-12">
                             <button class="btn btn-primary btn-sm">ثبت</button>
                         </section>
@@ -56,5 +65,13 @@
         </section>
     </section>
 </section>
+
+@endsection
+@section('script')
+
+    <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('body');
+    </script>
 
 @endsection
